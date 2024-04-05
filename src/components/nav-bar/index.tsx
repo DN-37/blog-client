d4a6277994fb2de3fafcacbcf178fe14b0424369
dn-37 <dn375@mail.ru>
2024-04-05 10:39:15 +0400

    created layout

---
+++ src/components/nav-bar/index.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import { BsPostcard } from "react-icons/bs"
+import { FaUsers } from "react-icons/fa"
+import { FiUsers } from "react-icons/fi"
+import { NavButton } from "../nav-button"
+
+export const NavBar: React.FC = () => {
+  return (
+    <nav>
+      <ul className="flex flex-col gap-5">
+        <li>
+          <NavButton href="/" icon={<BsPostcard />}>
+            Посты
+          </NavButton>
+        </li>
+        <li>
+          <NavButton href="/" icon={<FiUsers />}>
+            Подписки
+          </NavButton>
+        </li>
+        <li>
+          <NavButton href="/" icon={<FaUsers />}>
+            Подписчики
+          </NavButton>
+        </li>
+      </ul>
+    </nav>
+  )
+}
